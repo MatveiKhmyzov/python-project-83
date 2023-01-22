dfbc0ed215d2d726c6368957d756fa7274293428
Matvei Khmyzov <matw.hmizov@yandex.ru>
2023-01-22 17:41:20 +0300

    Fix linter errors

--- page_analyzer/tests/test_page_analyzer.py
+++ page_analyzer/tests/test_page_analyzer.py
@@ -17,11 +17,13 @@ def test_index(client):
     response = client.get("/")
     page = response.data.decode()
     assert '<title>Анализатор страниц</title>' in page
-    assert '<a class= "navbar-brand text-white" href="/">Анализатор страниц</a>' in page
+    assert '<a class= "navbar-brand text-white" ' \
+           'href="/">Анализатор страниц</a>' in page
     assert '<a class="nav-link text-white-20" href="#">Сайты</a>' in page
     assert '<h1 class="display-3">Анализатор страниц</h1>' in page
-    assert '<p class="lead">Бесплатно проверяйте сайты на SEO пригодность</p>' in page
+    assert '<p class="lead">Бесплатно проверяйте сайты ' \
+           'на SEO пригодность</p>' in page
     assert '<input class="form-control form-control-lg" type="text" name="#"' \
            ' placeholder="https://www.example.com">' in page
-    assert '<input class="btn btn-primary btn-lg ms-3 px-5 text-uppercase mx-3"' \
-           ' type="submit" value="Проверить">' in page
+    assert '<input class="btn btn-primary btn-lg ms-3 px-5 text-uppercase ' \
+           'mx-3" type="submit" value="Проверить">' in page
