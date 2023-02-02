2d95919333151e7bcb9b93dc1c214e0f75b3d493
Matvei Khmyzov <matw.hmizov@yandex.ru>
2023-02-02 11:15:15 +0300

    Fixed error in flash message#2

--- page_analyzer/app_handlers.py
+++ page_analyzer/app_handlers.py
@@ -57,8 +57,8 @@ def add_url():
             errors=errors
         )
     else:
-        add_url_record(url_fields_dct)
         flash('Страница успешно добавлена', 'alert-success')
+        add_url_record(url_fields_dct)
         url_record = get_url_by_name(url_fields_dct['url'])
         id = url_record['id']
         return redirect(url_for('get_one_url', id=id))
